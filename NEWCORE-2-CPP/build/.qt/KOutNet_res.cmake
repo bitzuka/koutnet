8efed1ba9d243f6d9078846c46010192807b95b6
bitzuka <matveipotuzhno@gmail.com>
2026-07-16 17:59:20 +0500

    Fix .gitignore: exclude build artifacts

--- NEWCORE-2-CPP/build/.qt/KOutNet_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "KOutNet")
-set(working_dir "/home/pixless/koutnet/NEWCORE-2-CPP")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/pixless/koutnet/NEWCORE-2-CPP/build/.qt/KOutNet_res.txt")
--- NEWCORE-2-CPP/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_KOutNet_FILE /home/pixless/koutnet/NEWCORE-2-CPP/build/KOutNet)
-set(__QT_DEPLOY_TARGET_KOutNet_TYPE EXECUTABLE)
